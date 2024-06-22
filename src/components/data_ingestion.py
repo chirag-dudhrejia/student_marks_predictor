5bf5032f97fd6d5175f561439a6ab1fae5116225
Chir@g159 <dchirag159@gmail.com>
2024-06-22 20:12:47 +0530

    done model training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,8 +6,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation, Data_transformation_config
+from src.components.model_trainer import ModelTrainer
 
-
+@dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join("artifacts", "train.csv")
     test_data_path: str = os.path.join("artifacts", "test.csv")
@@ -51,4 +52,6 @@ if __name__ == "__main__":
     obj = DataIngestion()
     train_data_path, test_data_path = obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+    model_trainer = ModelTrainer()
+    model_trainer.initiate_model_trainer(train_arr, test_arr)
